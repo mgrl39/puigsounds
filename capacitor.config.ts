12ca7d774bcdbf304d3e8bcf8a9eeb2c8613f9b1
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-02-20 22:32:26 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'puigsounds',
+  webDir: 'dist'
+};
+
+export default config;
