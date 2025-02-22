2fe03904cf2bb8769a634630e2bbf65eecfb523d
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-02-23 00:48:38 +0100

    Actualizado el diseño del login con versiones móvil y desktop, añadidos nuevos componentes y estilos

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,13 @@
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import './styles/general.css'; // Importa el CSS de escritorio
 </script>
 
 <style>
 /* Estilo global */
 .global-background {
-  background: #ff7979;
-  
+  background: #ff7979; /* Color de fondo por defecto */
   box-shadow: 0px 4px 4px 0px rgba(255, 179, 179, 0.25) inset;
   min-height: 100vh; /* Asegura que cubra toda la altura */
 }
--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,22 +1,61 @@
 <template>
   <ion-page>
     <ion-content class="ion-padding">
-      <div class="login-container">
-        <!-- <img src="@/assets/logo.png" alt="Logo" class="logo"/> -->
-        <ion-input label="Email" v-model="email" type="email" required></ion-input>
-        <ion-input label="Contraseña" v-model="password" type="password" required></ion-input>
-        <ion-button expand="full" @click="login">Iniciar Sesión</ion-button>
+      <img src="../../../assets/images/login-imgs/background/green.png" alt="Background" class="background-img mobile-only mobile-background"/>
+
+      <!-- Versión móvil -->
+      <div class="login-container mobile-only">
+        <div class="logo-container">
+          <img src="../../../favicon.png" alt="Logo" class="logo"/>
+        </div>
+        <PuigSounds />
+        <password-input placeholder="Username" type="text" v-model="email"></password-input><br>
+        <password-input placeholder="Password" type="password" v-model="password"></password-input>
+        <Submit>
+          Login
+        </Submit>
         <ion-text>
-          <a href="/password-reset">¿Olvidaste tu contraseña?</a>
+          <p class="forgot-password">test?</p>
         </ion-text>
+        <SeparatorLine />
+        <ion-text>
+          <p class="signup-text">¿No tienes una cuenta? <router-link to="/signup" class="signup-link">Regístrate</router-link></p>
+        </ion-text>
+      </div>
+
+      <!-- Versión desktop -->
+      <div class="login-container desktop-only" style="background-image: url('../../../assets/images/login-imgs/background/green.png'); background-size: cover; background-position: center;">
+        <div class="login-right">
+          <div class="logo-container">
+            <img src="../../assets/favicon.png" alt="Logo" class="logo"/>
+          </div>
+          <password-input placeholder="Usuario" type="text" v-model="email"></password-input>
+          <password-input placeholder="Contraseña" type="password" v-model="password"></password-input><br>
+          <checkBox>
+            Recordarme
+          </checkBox>
+          <Submit>
+          Login
+        </Submit>
+          <ion-text>
+            <p class="forgot-password">¿Olvidaste tu contraseña?</p>
+          </ion-text>
+          <ion-text>
+            <p class="signup-text">¿No tienes una cuenta? <router-link to="/signup" class="signup-link">Regístrate</router-link></p>
+          </ion-text>
+        </div>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup>
+import '../../styles/general.css';
 import { ref } from 'vue';
-import { IonPage, IonContent, IonInput, IonButton, IonText } from '@ionic/vue';
+import { IonPage, IonContent, IonButton, IonText } from '@ionic/vue';
+import PasswordInput from '@/views/components/Inputs/PasswordInput.vue';
+import Submit from '@/views/components/Inputs/Submit.vue';
+import PuigSounds from '@/views/components/ui/PuigSounds.vue';
 const email = ref('');
 const password = ref('');
 
@@ -30,9 +69,34 @@ const login = () => {
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 10px;
+  width: 100%;
+  height: 100%;
 }
+
+.background-img {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+}
+
+.logo-container {
+  margin: 20px 0;
+}
+
 .logo {
-  width: 100px;
+  width: 150px;
+  height: auto;
+}
+
+.login-button {
+  width: 368px;
+  height: 54px;
+  margin: 20px 0;
+}
+
+/* Estilo adicional para la versión desktop */
+.desktop-only {
+  background-size: cover;
+  background-position: center;
 }
 </style>
