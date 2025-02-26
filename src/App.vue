9c6d93e6ff40531ef306afc6c4e7a6e518977c9d
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-02-26 03:16:55 +0100

    feat: Añadir estilos globales para el fondo oscuro en App.vue - Implementación de tema oscuro consistente en la aplicación

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,14 @@
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import './styles/general.css'; // Importa el CSS de escritorio
 </script>
+<style>
+/* Estilos globales */
+ion-content {
+  --background: #131313; /* Color de fondo por defecto */
+}
 
-
+.global-background {
+  --ion-background-color: #131313;
+  min-height: 100vh;
+}
+</style>
