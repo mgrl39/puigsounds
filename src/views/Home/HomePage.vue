1666f591f10bf64ff4e1ebd8a8dca49f9d8409ac
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-02-27 01:57:39 +0100

    changed Home

--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -1,12 +1,22 @@
 <template>
   <BaseLayout>
     <template #main-content>
-      <MusicPlayer />
-      <NextBellIn />
-      <ChampionsBanner 
-        class="bottom-banner"
-        @click="handleChampionsClick"
-      />
+      <div class="home-container">
+        <div class="left-content">
+          <MusicPlayer class="music-player" />
+          <NextBellIn class="next-bell" />
+          <ChampionsBanner 
+            class="bottom-banner"
+            @click="handleChampionsClick"
+          />
+        </div>
+        <div class="right-content">
+          <ion-img 
+            src="../../../assets/images/logos/puig-love2.png"
+            class="heart-background"
+          ></ion-img>
+        </div>
+      </div>
     </template>
   </BaseLayout>
 </template>
@@ -45,6 +55,58 @@ import MusicPlayer from '../components/ui/MusicPlayer.vue';
 </script>
 
 <style scoped>
+.home-container {
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+
+.left-content {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+}
+
+.right-content {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+}
+
+.student-image {
+  max-height: 120%;
+  max-width: 120%;
+  opacity: 1;
+  transform: scale(1.2);
+  position: relative;
+  z-index: 2;
+}
+
+.heart-background {
+  position: absolute;
+  width: 250px;
+  height: 250px;
+  opacity: 0.4;
+  z-index: 1;
+  transform: scale(1.5);
+}
+
+.music-player {
+  margin-bottom: 1rem;
+}
+
+.next-bell {
+  margin-bottom: auto;
+}
+
+.bottom-banner {
+  margin-top: 1rem;
+}
+
 .main-section {
   flex: 1;
   min-width: 0;
@@ -67,4 +129,19 @@ import MusicPlayer from '../components/ui/MusicPlayer.vue';
   margin-left: 20px;
 }
 
+@media (max-width: 1210px) {
+  .heart-background {
+    display: none;
+  }
+}
+
+@media (max-width: 768px) {
+  .home-container {
+    flex-direction: column;
+  }
+  
+  .right-content {
+    display: none;
+  }
+}
 </style> 
